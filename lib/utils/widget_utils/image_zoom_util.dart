a3b3cba6191ca73e0ce281c58f6ee74b13700297
naz <n.zughaibi@gmail.com>
2019-05-04 12:23:28 -0400

    Added navigation up button since IOS devices don't have a clear back button.

--- lib/utils/widget_utils/image_zoom_util.dart
+++ lib/utils/widget_utils/image_zoom_util.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:photo_view/photo_view.dart';
 
@@ -9,17 +12,43 @@ class ImageZoom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Container(
-        constraints: BoxConstraints.expand(
-          height: MediaQuery.of(context).size.height,
+    return Stack(
+      children: <Widget>[
+        Container(
+            constraints: BoxConstraints.expand(
+              height: MediaQuery
+                  .of(context)
+                  .size
+                  .height,
+            ),
+            child: PhotoView(
+                key: Key("image_zoom"),
+                heroTag: url,
+                imageProvider: NetworkImage(
+                  url,
+                )
+            )
         ),
-        child: PhotoView(
-          key: Key("image_zoom"),
-          heroTag: url,
-          imageProvider: NetworkImage(
-            url,
-          )
+        new Positioned( //Place it at the top, and not use the entire screen
+          top: 0.0,
+          left: 0.0,
+          right: 0.0,
+          child: AppBar(
+            automaticallyImplyLeading: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            leading: new IconButton(
+              icon: new Icon(
+                Platform.isAndroid ? Icons.arrow_back : Icons.arrow_back_ios,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
         )
+      ],
     );
   }
 }
